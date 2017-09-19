c30d1aa4dbd2dd0198ee871d2ddea5468f651e6e
Trevor Hartman <trevorhartman@gmail.com>
2017-09-19 10:08:43 -0600

    Make sure mas is installed

--- .macapps.sh
+++ .macapps.sh
@@ -1,8 +1,8 @@
 # App store {{{
 
-mas install 918858936 # Airmail 3 (3.2.1)
+brew install mas
+mas install 918858936 # Airmail 3
 mas install 413857545 # Divvy (1.5)
-mas install 957734279 # Toggl Desktop
 mas install 409789998 # Twitter
 mas install 414030210 # LimeChat
 mas install 443987910 # 1Password - Password Manager and Secure Wallet
