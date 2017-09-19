0798036045590301f6d6d76dfd807fb95f19330d
Trevor Hartman <trevorhartman@gmail.com>
2017-09-19 10:50:34 -0600

    Try disabling substitutions for macos

--- .macos.sh
+++ .macos.sh
@@ -1,10 +1,28 @@
 # macOS Settings {{{
+
 # System: disable spelling
-defaults write -g NSAutomaticSpelling -bool false
-# System: disable smart quotes
-defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
-# System: disable smart dashes
-defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+# defaults write -g NSAutomaticSpelling -bool false
+# # System: disable smart quotes
+# defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+# # System: disable smart dashes
+# defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+#
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
 # TODO: also have to disable quotes/spelling in individual apps
 
 # only show apps that are open
