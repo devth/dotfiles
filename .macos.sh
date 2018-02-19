ce3101d23ce027876466e7b361000c07644b52c0
Trevor Hartman <trevorhartman@gmail.com>
2018-02-19 12:22:49 -0700

    Configure a bunch of stuff for macos

--- .macos.sh
+++ .macos.sh
@@ -1,3 +1,4 @@
+
 # macOS Settings {{{
 
   # System: disable spelling
@@ -38,6 +39,37 @@
   ln -sfn ~/Dropbox/dotfiles/.oh-my-zsh ~/.oh-my-zsh
   ln -sfn ~/Dropbox/dotfiles/.zshrc ~/.zshrc
 
+  # Standby delay to 24 hours
+  sudo pmset -a standbydelay 86400
+
+  # Disable transparency in the menu bar and elsewhere on Yosemite
+  defaults write com.apple.universalaccess reduceTransparency -bool true
+
+  # Menu bar: hide the Time Machine and User icons
+  defaults -currentHost write dontAutoLoad -array \
+    "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+    "/System/Library/CoreServices/Menu Extras/Clock.menu" \
+    "/System/Library/CoreServices/Menu Extras/User.menu"
+  defaults write com.apple.systemuiserver menuExtras -array \
+    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+    "/System/Library/CoreServices/Menu Extras/Battery.menu"
+
+  # Expand save panel by default
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+  # Expand print panel by default
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+  # Disable the “Are you sure you want to open this application?” dialog
+  defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+  # Restart automatically if the computer freezes
+  sudo systemsetup -setrestartfreeze on
+
+
 # }}}
 
 # Global Shortcuts {{{
@@ -54,6 +86,8 @@
   # Notification center: F12
 
   # Remap capslock to control
+  defaults write -g com.apple.keyboard.modifiermapping.1452-594-0 -array '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'
+
 
   # cmd + F1 show terminal
 
@@ -63,12 +97,55 @@
 
 # iTerm {{{
 
-  # Load iterm2 colors
-  # /Users/thartman/Dropbox/dotfiles/iterm2\ Colors
+  # Don’t display the annoying prompt when quitting iTerm
+  defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-  # Set default shell to zsh
 
-  # Install Solarized profiles and set it to Dark
+  # TODO tell iTerm to load prefs from ~/Dropbox/dotfiles/iterm2
+
+# }}}
+
+# Mac App Store {{{
+
+  # Enable the WebKit Developer Tools in the Mac App Store
+  defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+  # Enable Debug Menu in the Mac App Store
+  defaults write com.apple.appstore ShowDebugMenu -bool true
+
+  # Enable the automatic update check
+  defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+  # Check for software updates daily, not just once per week
+  defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+  # Download newly available updates in background
+  defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+  # Install System data files & security updates
+  defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+  # Automatically download apps purchased on other Macs
+  defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
+
+  # Turn on app auto-update
+  defaults write com.apple.commerce AutoUpdate -bool true
+
+  # Allow the App Store to reboot machine on macOS updates
+  defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+# }}}
+
+
+# Google Chrome & Google Chrome Canary {{{
+
+# Use the system-native print preview dialog
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+# Expand the print dialog by default
+defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
 
 # }}}
 
