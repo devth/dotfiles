7937aef12762c41029ff17cd0ad7f3558be49193
Trevor Hartman <trevorhartman@gmail.com>
2018-02-19 20:53:04 -0700

    Eschew homebrew nvm

--- .apps.sh
+++ .apps.sh
@@ -38,7 +38,7 @@ echo "NOTE: Install Dropbox and let it sync before running this"
   brew install ruby
 
   # nodejs
-  brew install nvm
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
   nvm install node
   nvm use node
   nvm alias default node
