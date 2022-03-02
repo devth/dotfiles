0507a8a99be3972d6c92158c974be2c828aaaf5c
Trevor Hartman <trevorhartman@gmail.com>
2022-03-02 15:20:44 -0700

    Remove mac apps

--- .macapps.sh
+++ .macapps.sh
@@ -1,15 +1,10 @@
 # App store {{{
 
 brew install mas
-mas install 918858936 # Airmail 3
-mas install 413857545 # Divvy (1.5)
-mas install 414030210 # LimeChat
-mas install 443987910 # 1Password - Password Manager and Secure Wallet
-mas install 406056744 # Evernote - stay organized
-mas install 416581096 # QuickCal
-mas install 417602904 # CloudApp: Capture&Share, GIFs, Videos, Screencasts
-mas install 497799835 # Xcode
-mas install 409183694 # Keynote
+# mas install 443987910 # 1Password - Password Manager and Secure Wallet
+# mas install 417602904 # CloudApp: Capture&Share, GIFs, Videos, Screencasts
+# mas install 497799835 # Xcode
+# mas install 409183694 # Keynote
 
 mas outdated
 
