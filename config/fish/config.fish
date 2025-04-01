5227720fd77e9e0910d526aff5696079f1db79f3
Trevor Hartman <th@devth.com>
2025-04-01 08:19:05 -0600

    Config asdf in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,16 @@ set -x LANG en_US.UTF-8
 eval "$(zoxide init zsh --cmd j)"
 fish_add_path /opt/homebrew/bin
 alias vim=nvim
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+  set _asdf_shims "$HOME/.asdf/shims"
+else
+  set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+  set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
