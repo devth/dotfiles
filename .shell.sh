8c47af4053fdb9062b81770ced2627102db6548e
Trevor Hartman <th@devth.com>
2025-04-01 08:13:56 -0600

    Add fish config

--- .shell.sh
+++ .shell.sh
@@ -2,3 +2,5 @@
 brew install fish
 sudo sh -c "echo $(which fish) >> /etc/shells"
 sudo chsh -s `which fish`
+
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
---
+++ config/fish/config.fish
@@ -0,0 +1,2 @@
+set -x LANG en_US.UTF-8
+eval "$(zoxide init zsh --cmd j)"
