1efa081403efd7e7602a0bd7ace37e1d8d75f859
Trevor Hartman <th@devth.com>
2025-03-31 18:41:27 -0600

    Update brew install command

--- .apps.sh
+++ .apps.sh
@@ -10,7 +10,7 @@ set -xe
   which brew > /dev/null 2>&1
   if [ $? -eq 1 ]; then
     xcode-select --install
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
   set -e
 # }}}
