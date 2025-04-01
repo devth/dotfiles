686568d1ae9b83937747bb8ffe84020952d8ee60
Trevor Hartman <th@devth.com>
2025-04-01 08:15:25 -0600

    Adjust fish

--- .shell.sh
+++ .shell.sh
@@ -2,5 +2,3 @@
 brew install fish
 sudo sh -c "echo $(which fish) >> /etc/shells"
 sudo chsh -s `which fish`
-
-set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,3 @@
 set -x LANG en_US.UTF-8
 eval "$(zoxide init zsh --cmd j)"
+fish_add_path /opt/homebrew/bin
