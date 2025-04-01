3c9da2a947c9663d55912d66b5d28a05bb957e70
Trevor Hartman <th@devth.com>
2025-04-01 08:19:35 -0600

    Asdf shell completion

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,3 +15,5 @@ if not contains $_asdf_shims $PATH
   set -gx --prepend PATH $_asdf_shims
 end
 set --erase _asdf_shims
+# ASDF shell completions
+asdf completion fish > ~/.config/fish/completions/asdf.fish
